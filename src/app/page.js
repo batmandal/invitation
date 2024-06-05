86e9509a2d384e7da14102c9d33a6e17a06bf15f
batmandal <batmandal98832221@gmail.com>
2024-06-05 23:49:30 +0800

    done

--- src/app/page.js
+++ src/app/page.js
@@ -47,8 +47,54 @@ export default function Invitation() {
       ></div>
       <div
         style={{ display: `${modalOpen == false ? "none" : "flex"}` }}
-        className="gorgeous w-[90vw]  h-[85vh] max-w-[430px] bg-white absolute z-20 rounded-2xl"
-      ></div>
+        className="gorgeous w-[90vw]  h-[85vh] max-w-[430px] bg-white absolute z-20 rounded-2xl px-[30px] py-[40px] flex-col items-center justify-center gap-[20px]"
+      >
+        <p
+          style={{ textShadow: "0 0 3px #FF0000, 0 0 5px #0000FF" }}
+          className=" text-white font-serif font-normal text-5xl txt-shad"
+        >
+          Урилга
+        </p>
+        <div className="w-full h-[30%] flex  justify-center">
+          {/* <Image src="/sticker1.webp" alt="sticker1" width={200} height={100} /> */}
+          {/* <Image src="/sticker2.webp" alt="sticker2" width={200} height={100} /> */}
+          <div
+            style={{
+              borderRadius: "12px",
+              width: "fit-content",
+              height: "fit-content",
+              overflow: "hidden",
+            }}
+          >
+            {" "}
+            <Image src="/profile.jpg" alt="sticker3" width={170} height={100} />
+          </div>
+        </div>
+        <div className="w-full h-[50%] flex flex-col gap-[10px] justify-between ">
+          <p
+            className=" text-center font-serif text-xl text-white font-semibold"
+            style={{ textShadow: "0 0 3px #FF0000, 0 0 5px #0000FF" }}
+          >
+            Эрхэм хүндэт таныг гэр бүлийн хамт Ууганбаярын Оюунзаяагийн "СЭВЛЭГ
+            ҮРГЭЭХ ЁСЛОЛ"-д зочлон ирж ерөөл буянаа харлана уу хэмээн хүндэтгэн
+            урьж байна.
+          </p>
+          <div>
+            <p
+              className="text-white font-serif text-xl font-medium"
+              style={{ textShadow: "0 0 3px #FF0000, 0 0 5px #0000FF" }}
+            >
+              Хэзээ: 2024 оны 6-р сарын 9нд
+            </p>
+            <p
+              className="text-white font-serif text-xl font-medium"
+              style={{ textShadow: "0 0 3px #FF0000, 0 0 5px #0000FF" }}
+            >
+              Хаана: Encanto town, ENCANTO SPORT COMPLEX, 11-р давхарт
+            </p>
+          </div>
+        </div>
+      </div>
       <p className="word text-blue-400 font-serif">Таньд урилга ирсэн байна</p>
       <div
         onClick={() => setModalOpen(true)}
