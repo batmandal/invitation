a7fdee09f6722f1a5cc4c5c22442f89bd45241cf
batmandal <batmandal98832221@gmail.com>
2024-06-06 00:27:09 +0800

    size

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Invitation() {
       ></div>
       <div
         style={{ display: `${modalOpen == false ? "none" : "flex"}` }}
-        className="gorgeous w-[90vw]  h-[100vh] max-h-[1000px] max-w-[430px]  absolute z-20 rounded-2xl px-[30px] py-[40px] flex-col items-center justify-center gap-[20px]"
+        className="gorgeous w-[90vw]  h-[130vh] max-h-[1000px] max-w-[430px]  absolute z-20 rounded-2xl px-[30px] py-[40px] flex-col items-center justify-center gap-[20px]"
       >
         <p
           style={{ textShadow: "0 0 3px #FF0000, 0 0 5px #0000FF" }}
